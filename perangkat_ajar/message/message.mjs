0010f62791649867f9b4d24ded332686122c404f
nurqahar <adha.nur.qahar@gmail.com>
2024-06-27 20:50:26 +0700

    modified files SiswaModel

--- perangkat_ajar/message/message.mjs
+++ perangkat_ajar/message/message.mjs
@@ -2,5 +2,13 @@ class Message {
   listen(port) {
     return `listen on: localhost:${port}`;
   }
+
+  notFound() {
+    return Error("404! NOT FOUND");
+  }
+
+  errLoadView() {
+    return Error("500! Error Loading View");
+  }
 }
 export default Message;
