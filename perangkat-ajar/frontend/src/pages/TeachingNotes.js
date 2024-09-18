93b89e5d283bb8c9c813507e74897fd547d6967c
nurqahar <adha.nur.qahar@gmail.com>
2024-09-18 22:17:44 +0700

    modified files

--- perangkat-ajar/frontend/src/pages/TeachingNotes.js
+++ perangkat-ajar/frontend/src/pages/TeachingNotes.js
@@ -32,34 +32,47 @@ const TeachingNotes = () => {
   const [subjects, setSubjects] = useState([]);
   const [teachingNotes, setTeachingNotes] = useState([]);
   const [teachers, setTeachers] = useState([]);
-  const [inputSearch, setInputSearch] = useState([]);
-
-  const handleEditClick = (e) => {
-    console.log("Edit button clicked");
-  };
+  const [resultSearchClass, setResultSearchClass] = useState();
 
   const searchTeachingNotes = async (event) => {
     event.preventDefault();
-    const date = event.target.date.value
-    const subjectId = event.target.subject.value
-    const classId = event.target.class.value
-    const resTeachingNotes = await axios.get(`${apiUrlTeachinNotes}?date=${date}&subject_id=${subjectId}&class_id=${classId}`)
-    console.log(resTeachingNotes.data);
-    
+
+    const resSubjects = await axios.get(`${apiUrlCourse}`);
+    const resClasses = await axios.get(`${apiUrlClasses}`);
+    const resStudents = await axios.get(`${apiUrlStudent}`);
+    setSubjects(resSubjects.data);
+    setClasses(resClasses.data);
+    setStudents(resStudents.data);
+
+    const date = event.target.date.value;
+    // const subjectId = event.target.class.value;
+    // const classId = event.target.class.value;
+    console.log(event.target.elements);
+    console.log(event.target.class.value);
+
+    // const resTeachingNotes = await axios.get(
+    // `${apiUrlTeachinNotes}?date=${date}&subject_id=${subjectId}&class_id=${classId}`
+    // );
+    // setTeachingNotes(resTeachingNotes.data);
+  };
+
+  const saveChanges = (event) => {
+    event.preventDefault();
+    console.log(event.target.elements);
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const resClasses = await axios.get(`${apiUrlClasses}`);
-        const resStudents = await axios.get(`${apiUrlStudent}`);
+        // const resStudents = await axios.get(`${apiUrlStudent}`);
         const resSubjects = await axios.get(`${apiUrlCourse}`);
         const resTeachers = await axios.get(`${apiUrlTeacher}`);
-        const resTeachingNotes = await axios.get(`${apiUrlTeachinNotes}`);
+        // const resTeachingNotes = await axios.get(`${apiUrlTeachinNotes}`);
 
-        setTeachingNotes(resTeachingNotes.data);
+        // setTeachingNotes(resTeachingNotes.data);
         setClasses(resClasses.data);
-        setStudents(resStudents.data);
+        // setStudents(resStudents.data);
         setSubjects(resSubjects.data);
         setTeachers(resTeachers.data);
         setLoading(false);
@@ -73,6 +86,7 @@ const TeachingNotes = () => {
 
   return (
     <Container className="mt-4">
+      {/* SEARCH */}
       <Row className="justify-content-center">
         <Col lg={6}>
           <Card border="primary">
@@ -83,21 +97,20 @@ const TeachingNotes = () => {
               <Form onSubmit={searchTeachingNotes}>
                 <Form.Group className="mb-3">
                   <FloatingLabel label="Date" className="mb-3">
-                    <Form.Control
-                      type="date"
-                      name="date"
-                    />
+                    <Form.Control type="date" name="date" />
                   </FloatingLabel>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
-                  <FloatingLabel label="Class" className="mb-3">
-                    <Form.Control
-                      type="text"
-                      placeholder="Class"
-                      name="class"
-                    />
-                  </FloatingLabel>
+                  <Form.Select name="class">
+                    <option>Pilih Kelas</option>
+                    <option value="1">X TOI 1</option>
+                    <option value="2">X TOI 2</option>
+                    <option value="3">XI TOI 1</option>
+                    <option value="4">XI TOI 2</option>
+                    <option value="5">XII TOI 1</option>
+                    <option value="6">XII TOI 2</option>
+                  </Form.Select>
                 </Form.Group>
 
                 <Form.Group className="mb-3">
@@ -120,72 +133,276 @@ const TeachingNotes = () => {
       {/* READ VALUE */}
       <Row className=" justify-content-center mt-5">
         <Col>
-          <Card>
-            <Card.Body>Teaching Notes</Card.Body>
-            <Form>
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="Content" className="mb-3">
-                  <Form.Control type="text" placeholder="Content" />
-                </FloatingLabel>
-              </Form.Group>
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="Date" className="mb-3">
-                  <Form.Control type="date" placeholder="Date" />
-                </FloatingLabel>
-              </Form.Group>
+          {teachingNotes.length !== 0 ? (
+            <Card>
+              <Card.Body>Teaching Notes</Card.Body>
+              <Form onSubmit={saveChanges}>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Class" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Class"
+                      value={
+                        teachingNotes.length !== 0
+                          ? classes[teachingNotes[0].class_id - 1].class
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Subject" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Subject"
+                      value={
+                        teachingNotes.length !== 0
+                          ? subjects[teachingNotes[0].subject_id - 1].subject
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Content" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Content"
+                      value={
+                        teachingNotes.length !== 0
+                          ? teachingNotes[0].content
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Date" className="mb-3">
+                    <Form.Control
+                      type="date"
+                      placeholder="Date"
+                      value={
+                        teachingNotes.length !== 0
+                          ? moment(teachingNotes[0].date).format("YYYY-MM-DD")
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="Time" className="mb-3">
-                  <Form.Control type="text" placeholder="Time" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Time" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Time"
+                      value={
+                        teachingNotes.length !== 0 ? teachingNotes[0].time : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="Total Content Time" className="mb-3">
-                  <Form.Control type="text" placeholder="Total Content Time" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Total Content Time" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Total Content Time"
+                      value={
+                        teachingNotes.length !== 0
+                          ? teachingNotes[0].total_content_time
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="School Year" className="mb-3">
-                  <Form.Control type="text" placeholder="School Year" />
-                </FloatingLabel>
-              </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="School Year" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="School Year"
+                      value={
+                        teachingNotes.length !== 0
+                          ? teachingNotes[0].school_year
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
 
-              <Form.Group className="m-auto mb-3 w-50">
-                <FloatingLabel label="Semester" className="mb-3">
-                  <Form.Control type="text" placeholder="Semester" />
-                </FloatingLabel>
-              </Form.Group>
-            </Form>
-          </Card>
-        </Col>
-      </Row>
-      <Row className=" justify-content-center mt-1">
-        <Col>
-          <Card>
-            <Table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Presence</th>
-                  <th>Notes</th>
-                  <th>Grade</th>
-                </tr>
-              </thead>
-              <tbody>
-              {teachingNotes.length !== 0 ? (
-                ):("")}
-                <tr>
-                  <td key={1}>index</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Card>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Semester" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Semester"
+                      value={
+                        teachingNotes.length !== 0
+                          ? teachingNotes[0].semester
+                          : ""
+                      }
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Presence</th>
+                      <th>Notes</th>
+                      <th>Grade</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {teachingNotes.length !== 0
+                      ? teachingNotes.map((teachingNote, index) => {
+                          return (
+                            <tr>
+                              <td key={index}>{index + 1}</td>
+                              <td>
+                                <Form.Group className="m-auto mb-2 w-50">
+                                  <Form.Control
+                                    type="text"
+                                    value={students && students[index].student}
+                                  />
+                                </Form.Group>
+                              </td>
+                              <td>
+                                <Form.Group className="m-auto mb-2 w-60">
+                                  <Form.Select>
+                                    <option value="HADIR">
+                                      {teachingNote.presence}
+                                    </option>
+                                    <option value="ALPA">ALPA</option>
+                                    <option value="SAKIT">SAKIT</option>
+                                    <option value="NO DATA">NO DATA</option>
+                                  </Form.Select>
+                                </Form.Group>
+                              </td>
+                              <td>
+                                <Form.Group className="m-auto mb-2 w-50">
+                                  <Form.Control
+                                    type="text"
+                                    value={teachingNote.notes}
+                                  />
+                                </Form.Group>
+                              </td>
+                              <td>
+                                <Form.Group className="m-auto mb-2 w-50">
+                                  <Form.Control
+                                    type="text"
+                                    value={teachingNote.grade}
+                                  />
+                                </Form.Group>
+                              </td>
+                            </tr>
+                          );
+                        })
+                      : ""}
+                  </tbody>
+                </Table>
+                <Button className="btn btn-warning">Save Changes</Button>
+              </Form>
+            </Card>
+          ) : (
+            <Card>
+              <Card.Body>Teaching Notes</Card.Body>
+              <Form onSubmit={saveChanges}>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Class" className="mb-3">
+                    <Form.Control type="text" placeholder="Class" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Subject" className="mb-3">
+                    <Form.Control type="text" placeholder="Subject" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Content" className="mb-3">
+                    <Form.Control type="text" placeholder="Content" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Date" className="mb-3">
+                    <Form.Control type="date" placeholder="Date" />
+                  </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Time" className="mb-3">
+                    <Form.Control type="text" placeholder="Time" />
+                  </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Total Content Time" className="mb-3">
+                    <Form.Control
+                      type="text"
+                      placeholder="Total Content Time"
+                    />
+                  </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="School Year" className="mb-3">
+                    <Form.Control type="text" placeholder="School Year" />
+                  </FloatingLabel>
+                </Form.Group>
+
+                <Form.Group className="m-auto mb-3 w-50">
+                  <FloatingLabel label="Semester" className="mb-3">
+                    <Form.Control type="text" placeholder="Semester" />
+                  </FloatingLabel>
+                </Form.Group>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Presence</th>
+                      <th>Notes</th>
+                      <th>Grade</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td></td>
+                      <td>
+                        <Form.Group className="m-auto mb-2 w-50">
+                          <Form.Control
+                            type="text"
+                            value={students && students[0].student}
+                          />
+                        </Form.Group>
+                      </td>
+                      <td>
+                        <Form.Group className="m-auto mb-2 w-60">
+                          <Form.Select>
+                            <option value="HADIR">HADIR</option>
+                            <option value="ALPA">ALPA</option>
+                            <option value="SAKIT">SAKIT</option>
+                            <option value="NO DATA">NO DATA</option>
+                          </Form.Select>
+                        </Form.Group>
+                      </td>
+                      <td>
+                        <Form.Group className="m-auto mb-2 w-50">
+                          <Form.Control type="text" />
+                        </Form.Group>
+                      </td>
+                      <td>
+                        <Form.Group className="m-auto mb-2 w-50">
+                          <Form.Control type="text" />
+                        </Form.Group>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Button className="btn btn-warning">Save Changes</Button>
+              </Form>
+            </Card>
+          )}
         </Col>
       </Row>
     </Container>
